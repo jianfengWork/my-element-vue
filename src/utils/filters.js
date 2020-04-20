e09477a64b3b67f2018985f14308001deb1377cd
summer <18855176517@163.com>
2020-04-20 19:44:57 +0800

    getBytes

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -7,3 +7,22 @@ import moment from 'moment'
 export function formatDate(timestamp) {
   return timestamp ? moment(timestamp * 1000).format('YYYY-MM-DD HH:mm:ss') : '-'
 }
+
+/**
+ * 转换金额展示
+ * @param val 支持数字、字符串
+ * @param digit 支持数字、字符串
+ * @returns {'10,000.00'}
+ */
+export function formatCash(val, digit) {
+  if (!val) val = 0
+  digit = digit >= 0 && digit <= 20 ? digit : 2
+  val = parseFloat((val + '').replace(/[^\d\.-]/g, '')).toFixed(digit) + ''
+  let l = val.split('.')[0].split('').reverse()
+  let r = val.split('.')[1]
+  let t = ''
+  for (let i = 0; i < l.length; i++) { 
+    t += l[i] + ((i + 1) % 3 === 0 && (i + 1) !== l.length ? ',' : '')
+  } 
+  return t.split('').reverse().join('') + '.' + r
+}
--- src/utils/transform.js
+++ src/utils/transform.js
@@ -1,21 +1,4 @@
-/**
- * 转换金额展示
- * @param val 支持数字、字符串
- * @param digit 支持数字、字符串
- * @returns {'10,000.00'}
- */
-export function formatCash(val, digit) {
-  if (!val) val = 0
-  digit = digit >= 0 && digit <= 20 ? digit : 2
-  val = parseFloat((val + '').replace(/[^\d\.-]/g, '')).toFixed(digit) + ''
-  let l = val.split('.')[0].split('').reverse()
-  let r = val.split('.')[1]
-  let t = ''
-  for (let i = 0; i < l.length; i++) { 
-    t += l[i] + ((i + 1) % 3 === 0 && (i + 1) !== l.length ? ',' : '')
-  } 
-  return t.split('').reverse().join('') + '.' + r
-}
+
 
 /**
  * 补零
@@ -43,4 +26,17 @@ export function uniqueArray(arr) {
     }
   }
   return temp
+}
+
+/**
+ * 字节长度
+ * @param String 支持字符串
+ * @returns {Number}
+ */
+export function getBytes(val) {
+  let len = val.length
+  for (let i in val) {
+    if (val.charCodeAt(i) > 255) len++
+  }
+  return len
 }
