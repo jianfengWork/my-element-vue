a84936c3b0de88eba00d5c4e4c735b2af84eac4e
summer <18855176517@163.com>
2020-05-08 11:36:25 +0800

    ZeroOrIntegerVoidLimit

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -12,4 +12,16 @@ export default {
       callback()
     }
   },
+  ZeroOrIntegerVoidLimit(rule, value, callback) { // (0 || 正整数) 有上限
+    const regExp = /^(0|\+?[1-9][0-9]*)$/
+    if (!value) {
+      callback(rule.msg || '不为空')
+    } else if (!regExp.test(value)) {
+      callback('请输入0或正整数')
+    } else if (Number(value) > rule.limit) {
+      callback('超过上限 ' + rule.limit)
+    } else {
+      callback()
+    }
+  },
 }
