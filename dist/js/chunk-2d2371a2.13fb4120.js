2125706e84ede478a290a680c727cacfa3a3a1c9
summer <18855176517@163.com>
2020-06-24 10:45:03 +0800

    add dist

---
+++ dist/js/chunk-2d2371a2.13fb4120.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2371a2"],{fa4e:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("404")])},a=[],c={},l=c,r=t("4023"),s=Object(r["a"])(l,u,a,!1,null,null,null);e["default"]=s.exports}}]);
--- vue.config.js
+++ vue.config.js
@@ -41,26 +41,26 @@ module.exports = {
     },
     plugins: [compress]
   },
-  configureWebpack: () => {
-    if (process.env.NODE_ENV !== 'production') return
-    return {
-      plugins: [
-        new PrerenderSPAPlugin({
-          // 这个目录只能有一级，如果目录层次大于一级，在生成的时候不会有任何错误提示，在预渲染的时候只会卡着不动。
-          staticDir: path.join(__dirname, 'dist'), // 生成文件的路径，也可以与webpakc打包的一致。
-          routes: ['/', '/front/cart'], // 对应自己的路由路径
-          renderer: new Renderer({ // 这个很重要，如果没有配置这段，也不会进行预编译
-            inject: {
-              foo: 'bar'
-            },
-            headless: false,
-            // 在 main.js 中 document.dispatchEvent(new Event('render-event'))，两者的事件名称要对应上。
-            renderAfterDocumentEvent: 'render-event'
-          })
-        }),
-      ],
-    }
-  },
+  // configureWebpack: () => {
+  //   if (process.env.NODE_ENV !== 'production') return
+  //   return {
+  //     plugins: [
+  //       new PrerenderSPAPlugin({
+  //         // 这个目录只能有一级，如果目录层次大于一级，在生成的时候不会有任何错误提示，在预渲染的时候只会卡着不动。
+  //         staticDir: path.join(__dirname, 'dist'), // 生成文件的路径，也可以与webpakc打包的一致。
+  //         routes: ['/', '/front/cart'], // 对应自己的路由路径
+  //         renderer: new Renderer({ // 这个很重要，如果没有配置这段，也不会进行预编译
+  //           inject: {
+  //             foo: 'bar'
+  //           },
+  //           headless: false,
+  //           // 在 main.js 中 document.dispatchEvent(new Event('render-event'))，两者的事件名称要对应上。
+  //           renderAfterDocumentEvent: 'render-event'
+  //         })
+  //       }),
+  //     ],
+  //   }
+  // },
   transpileDependencies: ['vue-clamp', 'resize-detector'], // vue-clamp组件需要
   css: { // 从js中提取css包
     extract: true
