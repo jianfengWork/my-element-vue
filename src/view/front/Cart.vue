bf44ca5b4d20ae2a69b0ac3828e088db87685924
summer <18855176517@163.com>
2020-07-28 16:11:18 +0800

    balls

--- src/view/front/Cart.vue
+++ src/view/front/Cart.vue
@@ -4,7 +4,7 @@
       <el-col :span="4" v-for="item in goodsList" :key="item.id">
         <el-image :src="item.img" fit="contain" />
         <div class="goods-name TC">{{item.name}} ¥{{item.price}}</div>
-        <div class="goods-button TC"><el-button type="primary" size="mini" @click="addCart(item)">加入购物车</el-button></div>
+        <div class="goods-button TC"><el-button type="primary" size="mini" @click="($event) => addCart(item, $event)">加入购物车</el-button></div>
       </el-col>
     </el-row>
     <div class="cart-wrap">
@@ -27,10 +27,32 @@
       <div class="cart-cont" v-else>暂无商品</div>
       <div class="total-price">总价：¥{{totalPrice}}</div>
     </div>
+    <!-- 小球动画 -->
+    <div class="ball-container">
+      <div v-for="(ball, index) in balls" :key="index">
+        <transition
+          @before-enter="beforeDrop"
+          @enter="dropping"
+          @after-enter="afterDrop">
+          <div class="ball-cont" v-show="ball.show">
+            <div class="inner inner-hook" :style="{'background': $store.state.themeColor}"></div>
+          </div>
+        </transition>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const innerClsHook = '.inner-hook'
+function createBalls() {
+  const balls = []
+  for (let i = 0; i < 10; i++) {
+    balls.push({ show: false })
+  }
+  return balls
+}
+
 export default {
   data() {
     return {
@@ -95,9 +117,11 @@ export default {
           name: '洗面奶',
         },
       ],
+      balls: createBalls(),
     }
   },
   created() {
+    this.dropBalls = []
     console.log('第一次进入keep-alive触发')
   },
   activated() {
@@ -123,7 +147,7 @@ export default {
     },
   },
   methods: {
-    addCart(item) {
+    addCart(item, evt) {
       let flag = false // 记录 存在
       for (let i in this.cartList) {
         if (this.cartList[i].id === item.id) { // 已存在，商品 +1
@@ -134,6 +158,12 @@ export default {
       }
       if (!flag) this.cartList.push(item) // 不存在，加入商品
       this.changeSelect()
+
+      if (evt.target.tagName === 'SPAN') {
+        this.drop(evt.currentTarget) // 取父级 button
+      } else {
+        this.drop(evt.target)
+      }
     },
     delGoods(item, index) {
       this.cartList.splice(index, 1)
@@ -171,6 +201,41 @@ export default {
           select: bol,
         }
       })
+    },
+    drop(el) {
+      for (let i = 0; i < this.balls.length; i++) {
+        const ball = this.balls[i]
+        if (!ball.show) {
+          ball.show = true
+          ball.el = el
+          this.dropBalls.push(ball)
+          return
+        }
+      }
+    },
+    beforeDrop(el) {
+      const ball = this.dropBalls[this.dropBalls.length - 1]
+      const rect = ball.el.getBoundingClientRect()
+      const x = rect.left - 300
+      const y = -(window.innerHeight - rect.top - 540)
+      el.style.display = ''
+      el.style.transform = el.style.webkitTransform = `translate3d(0,${y}px,0)`
+      const inner = el.querySelector(innerClsHook)
+      inner.style.transform = inner.style.webkitTransform = `translate3d(${x}px,0,0)`
+    },
+    dropping(el, done) {
+      this._reflow = document.body.offsetHeight // 浏览器重绘
+      el.style.transform = el.style.webkitTransform = `translate3d(0,0,0)`
+      const inner = el.querySelector(innerClsHook)
+      inner.style.transform = inner.style.webkitTransform = `translate3d(0,0,0)`
+      el.addEventListener('transitionend', done)
+    },
+    afterDrop(el) {
+      const ball = this.dropBalls.shift()
+      if (ball) {
+        ball.show = false
+        el.style.display = 'none'
+      }
     },
   },
 }
@@ -214,6 +279,21 @@ export default {
     .total-price {
       margin-top: 20px;
     }
+  }
+  .ball-container {
+    .ball-cont {
+      position: fixed;
+      left: 300px;
+      bottom: 540px;
+      z-index: 200;
+      transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41); // 贝塞尔曲线
+      .inner {
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        transition: all 0.4s linear;
+      }
+    }       
   }
 }
 </style>
