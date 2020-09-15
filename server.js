d2d2e5306833a7d983ba0ba8a822096f47c204cf
summer <18855176517@163.com>
2020-09-15 19:20:39 +0800

    add server

---
+++ server.js
@@ -0,0 +1,9 @@
+const Koa = require('koa')
+const static = require('koa-static')
+
+const server = new Koa();
+server.listen(3030);
+
+server.use(static('./public/static/', {
+  maxage: 86400 * 1000, // 缓存有效期
+}));
