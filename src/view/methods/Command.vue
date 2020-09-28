eaa7c3a84848b81b96afff4019764e4701e24cff
summer <18855176517@163.com>
2020-09-28 14:55:21 +0800

    computed

--- src/view/methods/Command.vue
+++ src/view/methods/Command.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>
+  <div class="my-command">
     <el-button type="primary" @click="show = !show">{{show ? '销毁' : '挂载'}}</el-button>
     <el-button v-if="show" v-append-text="`hello ${number}`" @click="number++">
       点我
     </el-button>
+    <!-- computed -->
+    <el-alert title="computed" type="success" :closable="false" class="MT20" />
+    <div class="computed">
+      <div class="MT20">
+        <span>firstname：</span>
+        <el-input v-model.trim="firstname" />
+      </div>
+      <div class="MT20">
+        <span>lastname：</span>
+        <el-input v-model.trim="lastname" />
+      </div>
+      <div class="MT20">
+        <span>familyname：</span>
+        <el-input v-model.trim="familyname" />
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -30,11 +46,39 @@ export default {
       }
     }
   },
+  computed: {
+    familyname: { // 需要 {{familyname}} 触发
+      get() {
+        return this.firstname + this.lastname
+      },
+      set(val) { // 设置 v-model 触发
+        console.log(val)
+        this.firstname = val[0] || ''
+        this.lastname = val.substring(1) || ''
+      },
+    },
+  },
   data() {
     return {
       number: 1,
-      show: true
+      show: true,
+      firstname: '建',
+      lastname: '峰',
     }
   }
 }
 </script>
+
+<style lang="scss">
+.my-command {
+  .el-input {
+    width: 300px;
+  }
+  .computed {
+    span {
+      display: inline-block;
+      width: 100px;
+    }
+  }
+}
+</style>
