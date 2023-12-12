b382ef44df4eed505a1bd297eccebc23a3867106
jianfeng <18855176517@163.com>
2023-12-12 17:01:01 +0800

    add: 字体

--- src/view/tools/SvgIcon.vue
+++ src/view/tools/SvgIcon.vue
@@ -4,6 +4,12 @@
     <svg-icon icon-class="wechat" />
     <el-alert title="icon" type="success" :closable="false" />
     <i class="icon-TeamRequest iconfont" />
+    <el-alert title="数字" type="success" :closable="false" />
+    <div style="font-family: 'DIN-Bold'; font-size: 30px;">abc123456789</div>
+    <div style="font-size: 30px;">abc123456789</div>
+    <el-alert title="英文" type="success" :closable="false" />
+    <div style="font-family: 'Helvetica'; font-size: 30px;">abc123456789</div>
+    <div style="font-size: 30px;">abc123456789</div>
   </div>
 </template>
 
